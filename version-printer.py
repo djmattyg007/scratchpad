2895da50a0308c6ba4a72f87a07099cc38311524
Matthew Gamble <git@matthewgamble.net>
2025-11-27 00:47:47 +1100

    better version printing

---
+++ version-printer.py
@@ -0,0 +1,37 @@
+import shlex
+import subprocess
+
+version_flag_cmds = (
+    "bash",
+    "pwsh",
+    "python3",
+    "perl",
+    "cargo",
+    "node",
+    "npm",
+    "yarn",
+    "git",
+    "git lfs",
+    "gh",
+    "jq",
+)
+
+version_sub_cmds = (
+    "go",
+    "docker",
+    "docker compose",
+)
+
+for cmd in version_flag_cmds:
+    print(">", cmd)
+    try:
+        subprocess.run((*shlex.split(cmd), "--version"))
+    except FileNotFoundError:
+        print("not found")
+
+for cmd in version_sub_cmds:
+    print(">", cmd)
+    try:
+        subprocess.run((*shlex.split(cmd), "version"))
+    except FileNotFoundError:
+        print("not found")
